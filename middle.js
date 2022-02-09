d0e4650fcee7d3ca3c70e609af85683d1f56e3fd
VinceEmond <vince.emond@icloud.com>
2022-02-09 23:24:41 +0000

    Intial commit: Creating middle file

---
+++ middle.js
@@ -0,0 +1,64 @@
+const eqArrays = function(array1, array2) {
+
+  //Check if parameters are not arrays
+  if (!(Array.isArray(array1) || !(Array.isArray(array2)))) {
+    return false;
+  }
+
+  //Check if arrays lengths are different
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  //Check if any items are differen in the array
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  //If no differnces found, return true
+  return true;
+
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const middle = function(numberAr) {
+
+  let middleElements = [];
+
+  // Arrays with one or two elements, Return an empty array.
+  if (numberAr.length <= 2) {
+    return middleElements;
+  }
+
+  //Find Middle index of array
+  let middleIndex = Math.floor(numberAr.length / 2);
+
+  // Even length --> return two values
+  // Odd length --> return single middle value
+  if (!(numberAr.length % 2)) {
+    middleElements.push(numberAr[[middleIndex - 1]]);
+    middleElements.push(numberAr[[middleIndex]]);
+  } else {
+    middleElements.push(numberAr[middleIndex]);
+  }
+
+  return middleElements;
+};
+
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
+
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);// => [3]
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
