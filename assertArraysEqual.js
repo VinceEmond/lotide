6748b8cef2dcfb99d7186a6ecc1064878e1050e4
VinceEmond <vince.emond@icloud.com>
2022-02-09 03:49:15 +0000

    File Created: assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+
+  //Check if parameters are not arrays
+  if (!(Array.isArray(array1) || !(Array.isArray(array2)))) {
+    return false;
+  }
+
+  //Check if arrays lengths are different
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  //Check if any items are differen in the array
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  //If no differnces found, return true
+  return true;
+
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // True
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // False
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // True
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // False
