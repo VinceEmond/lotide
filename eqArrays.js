3808cce1e95865c7f05aaf2bbf1afb9fd6d0e66d
VinceEmond <vince.emond@icloud.com>
2022-02-09 03:03:16 +0000

    Initial Commit: Created eqArrays file

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+const assertEqual = (actual, expected) => {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(array1, array2) {
+
+  //Compare array length
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  //Compare all items in arrays
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  //If no differnces found, return true
+  return true;
+
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
