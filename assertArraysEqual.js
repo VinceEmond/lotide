19fec0e4a7a09a3e0815c384a5969e51e5f3b1c9
VinceEmond <vince.emond@icloud.com>
2022-02-14 23:29:46 +0000

    Refactored code to use seperate test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,30 +1,4 @@
-
 const eqArrays = require("./eqArrays.js");
-
-
-// const eqArrays = function(array1, array2) {
-
-//   //Check if parameters are not arrays
-//   if (!(Array.isArray(array1) || !(Array.isArray(array2)))) {
-//     return false;
-//   }
-
-//   //Check if arrays lengths are different
-//   if (array1.length !== array2.length) {
-//     return false;
-//   }
-
-//   //Check if any items are differen in the array
-//   for (let i = 0; i < array1.length; i++) {
-//     if (array1[i] !== array2[i]) {
-//       return false;
-//     }
-//   }
-
-//   //If no differnces found, return true
-//   return true;
-
-// };
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -33,6 +7,5 @@ const assertArraysEqual = function(array1, array2) {
     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-
 
 module.exports = assertArraysEqual;
---
+++ test/middleTest.js
@@ -0,0 +1,11 @@
+const middle = require("../middle.js");
+const assertArraysEqual = require("../assertArraysEqual.js");
+
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
+
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);// => [3]
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
